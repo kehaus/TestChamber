2d304855c01b4c339b7da5cb82e3b31ad8e91fdf
kehaus <tv_junkie_4ever@hotmail.com>
2019-04-30 03:59:12 -0400

    added precursor inlet measurements at 50C and 77C

--- analysis/precursor_inlet_test.py
+++ analysis/precursor_inlet_test.py
@@ -16,8 +16,12 @@ import matplotlib.pyplot as plt
 
 
 
-### measurement data
-valve_pos_open = np.array([
+# ====================================
+# measurement data
+# ====================================
+
+## precursor temperature 22°C
+valve_pos_open_22C = np.array([
 	0,
 	50,
 	100,
@@ -30,7 +34,7 @@ valve_pos_open = np.array([
 	300,
 	350,
 ])
-valve_pos_close = np.array([
+valve_pos_close_22C = np.array([
 	350,
 	250,
 	200,
@@ -42,7 +46,7 @@ valve_pos_close = np.array([
 	40
 ])
 
-p_open = np.array([		# [torr]
+p_open_22C = np.array([		# [torr]
 	2.1,
 	2.1,
 	2.1,
@@ -55,7 +59,7 @@ p_open = np.array([		# [torr]
 	12,
 	12
 ]) * 10**-7
-p_close = np.array([	# [torr]
+p_close_22C = np.array([	# [torr]
 	12,
 	12,
 	12,
@@ -68,31 +72,160 @@ p_close = np.array([	# [torr]
 ]) * 10**-7
 
 
+## precursor temperature 50°C
+valve_pos_open_50C = np.array([
+	0,
+	20,
+	40,
+	60,
+	80,
+	100,
+	120,
+	140,
+	160,
+	180,
+	200,
+	220,
+	240,
+	260,
+	280,
+	300
+])
 
-### plot results
+
+valve_pos_close_50C = np.array([
+	300,
+	250,
+	200,
+	150,
+	100,
+	50,
+	0
+])
+
+p_open_50C = np.array([		# [torr]
+	0.34,
+	0.34,
+	0.34,
+	0.36,
+	0.41,
+	0.63,
+	1.0,
+	1.7,
+	6.9,
+	11,
+	12,
+	13,
+	14,
+	14,
+	14,
+	14
+]) * 10**-7
+
+p_close_50C = np.array([		# [torr]
+	14,
+	14,
+	13,
+	4.4,
+	0.7,
+	0.58,
+	0.45
+]) * 10**-7
+
+
+
+## precursor temperature 77°C
+valve_pos_open_77C = np.array([
+	0,
+	20,
+	40,
+	60,
+	80,
+	100,
+	120,
+	140,
+	160,
+	180,
+	200,
+	220,
+	240,
+	260,
+	280,
+	300
+])
+
+
+valve_pos_close_77C = np.array([
+	300,
+	250,
+	200,
+	150,
+	100,
+	50,
+	0
+])
+
+p_open_77C = np.array([		# [torr]
+	0.33,
+	0.32,
+	0.33,
+	0.33,
+	0.40,
+	0.65,
+	1.2,
+	2.4,
+	9.3,
+	14,
+	17,
+	17,
+	18,
+	18,
+	18,
+	18
+]) * 10**-7
+
+p_close_77C = np.array([		# [torr]
+	18,
+	18,
+	17,
+	5.6,
+	0.47,
+	0.38,
+	0.36
+]) * 10**-7
+
+
+# ====================================
+# plots
+# ====================================
 ax1_dct = {
-  'xlabel':   'needle valve position [valve scale]',
+  'xlabel':   'Needle valve position [valve scale]',
   'ylabel':   'Pressure [torr]'
 }
 
 fig1 = plt.figure(figsize=(8,5))
 ax1 = fig1.add_subplot(111, **ax1_dct)
-ax1.plot(valve_pos_open,p_open, label='open valve')
-ax1.plot(valve_pos_close, p_close, label='close valve')
+ax1.plot(valve_pos_open_22C,p_open_22C, label='open valve')
+ax1.plot(valve_pos_close_22C, p_close_22C, label='close valve')
 ax1.legend(loc=0)
 ax1.yaxis.set_major_formatter(mpl.ticker.StrMethodFormatter('{x:.1e}'))
 plt.grid()
 
 
-#ax2_dct = {
-#  'xlabel': 'Current I [A]',
-#  'ylabel': 'Voltage V [V]'
-#}
+ax2_dct = {
+  'xlabel':   'Needle valve position [valve scale]',
+  'ylabel':   'Pressure [torr]'
+}
 
-#fig2 = plt.figure()
-#ax2 = fig2.add_subplot(111, **ax2_dct)
-#ax2.plot(I,V, c='orange')
-
+fig2 = plt.figure(figsize=(8,5))
+ax2 = fig2.add_subplot(111, **ax2_dct)
+ax2.plot(valve_pos_open_50C,p_open_50C, label='open valve, 50C')
+ax2.plot(valve_pos_close_50C, p_close_50C, label='close valve 50C')
+ax2.plot(valve_pos_open_77C,p_open_77C, label='open valve 77C')
+ax2.plot(valve_pos_close_77C, p_close_77C, label='close vale, 77C')
+ax2.legend(loc=0)
+ax2.yaxis.set_major_formatter(mpl.ticker.StrMethodFormatter('{x:.1e}'))
+plt.grid()
 
 plt.show()
 
