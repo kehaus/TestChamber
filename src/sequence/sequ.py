7cbe97aa7b0ab50131eba5a578bd6fdf1617179a
Kevin Hauser <kevinhauser@g.harvard.edu>
2019-05-20 04:57:34 +0000

    included first example of WorkerTask use case in sequence/sequ.py

--- src/sequence/sequ.py
+++ src/sequence/sequ.py
@@ -10,10 +10,50 @@ __author__ = "kha"
 
 
 
-from ..daq.daqunit import DAQUnitbase
-from ..daq.sm7022 import sm7022
+import queue
 
-#dd = DAQUnitBase()
-#sm = SM7022(dd)
+from ..daq.daqunit import DAQUnitBase
+from ..daq.sm7022 import SM7022
+from ..daq.pkr251 import PKR251
+from ..daq.rs880varian import RS880Varian
+
+from ..util.worker_thread import WorkerThread, WorkerTask
+
+dd = DAQUnitBase()
+sm = SM7022(dd)
+pkr = PKR251(dd)
+rs = RS880Varian(dd)
+
+
+###
+
+pkr_wt = WorkerTask(pkr.get_pressure, continuous=True, save=True, base_name='pkr251')
+rs_wt = WorkerTask(rs.get_pressure, continuous=True, save=True, base_name='rs880')
+
+q = queue.Queue()
+
+q.put(pkr_wt)
+q.put(rs_wt)
+
+w = WorkerThread(q)
+
+
+
+
+
+
+#class PKR251Task(WorkerTask):
+#    """ """
+
+    
+
+
+
+
+
+
+
+
+
 
 
